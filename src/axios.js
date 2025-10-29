222d00cabfa5a248898fa24737d6a41f69a0f354
kevjo <kevvjoo@gmail.com>
2025-10-29 14:06:11 +0700

    fix: adjust base api route name

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://simple-sales-system.test/api',
+    baseURL: 'http://simple-sales-system-api.test/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
