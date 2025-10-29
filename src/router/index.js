30ad73da55a0106434fa7f0ce024907378c0cbe6
kevjo <kevvjoo@gmail.com>
2025-10-29 20:47:45 +0700

    feat: sales order create, read

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Customers from '../views/Customers.vue';
 import Products from '../views/Products.vue';
+import SalesOrders from '../views/SalesOrders.vue';
 
 const routes = [
     { path: '/', redirect: '/products' },
     { path: '/customers', component: Customers },
     { path: '/products', component: Products },
+    { path: '/salesOrders', component: SalesOrders },
 ];
 
 const router = createRouter({
